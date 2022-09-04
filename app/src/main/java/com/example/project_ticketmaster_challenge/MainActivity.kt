22f0e5e4e25a8dc1e6979b75c24e5dbee3377efd
haykIsayan <haykisayan@gmail.com>
2022-09-04 12:48:22 -0700

    Use ktx extensions for easier access to views through xml ids.

--- app/src/main/java/com/example/project_ticketmaster_challenge/MainActivity.kt
+++ app/src/main/java/com/example/project_ticketmaster_challenge/MainActivity.kt
@@ -2,15 +2,14 @@ package com.example.project_ticketmaster_challenge
 
 import android.os.Bundle
 import android.view.View
-import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.project_ticketmaster_challenge.model.EventModel
 import com.example.project_ticketmaster_challenge.ui.DiscoverEventsAdapter
 import com.example.project_ticketmaster_challenge.ui.DiscoverEventsViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -18,15 +17,10 @@ class MainActivity : ComponentActivity() {
     private val discoverEventsViewModel by viewModels<DiscoverEventsViewModel>()
     private val discoverEventsAdapter = DiscoverEventsAdapter()
 
-    private lateinit var noEventsTextView: TextView
-    private lateinit var discoverEventsRecyclerView: RecyclerView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        noEventsTextView = findViewById(R.id.noEventsTextView)
-        discoverEventsRecyclerView = findViewById(R.id.discoverEventsRecyclerView)
         initDiscoverEventsRecyclerView()
 
         discoverEventsViewModel.getDiscoverEvents().observe(this, ::onDiscoverEvents)
