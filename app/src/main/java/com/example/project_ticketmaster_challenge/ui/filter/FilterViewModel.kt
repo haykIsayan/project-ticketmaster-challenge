60f736863f0d8cdc6fca348bd95b5cc333472e87
haykIsayan <haykisayan@gmail.com>
2022-09-09 19:49:26 -0700

    [challenge-v2-improved-ui] Move filter package to ui package level.

--- app/src/main/java/com/example/project_ticketmaster_challenge/ui/filter/FilterViewModel.kt
+++ app/src/main/java/com/example/project_ticketmaster_challenge/ui/filter/FilterViewModel.kt
@@ -1,4 +1,4 @@
-package com.example.project_ticketmaster_challenge.ui.search.filter
+package com.example.project_ticketmaster_challenge.ui.filter
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
